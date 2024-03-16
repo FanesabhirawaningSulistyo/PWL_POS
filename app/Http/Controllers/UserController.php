40aabfd2de1edb9c405c837f6a00bd8252976633
fanesabhirawaning <fanesahbi22@gmail.com>
2024-03-16 20:32:43 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,20 +11,19 @@ class UserController extends Controller
     public function index()
     {
         // Data yang akan dimasukkan ke dalam tabel m_user
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345') // Mengenkripsi password sebelum disimpan
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345') // Mengenkripsi password sebelum disimpan
+        // ];
 
-        // Menyimpan data ke dalam tabel m_user menggunakan model UserModel
-        UserModel::create($data);
-
-        // Mengambil semua data pengguna dari tabel m_user
-        $users = UserModel::all();
-
-        // Mengembalikan view 'user' dengan data pengguna
+        // // Menyimpan data ke dalam tabel m_user menggunakan model UserModel
+        // UserModel::create($data);
+        
+        $users = UserModel::findOr(20,['username','nama'], function(){
+        abort(404);
+    });
         return view('user', ['data' => $users]);
     }
 }

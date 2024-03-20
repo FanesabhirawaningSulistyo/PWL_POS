584c86e3530d794efcdac8cf460bcd76d3b88ab5
fanesabhirawaning <fanesahbi22@gmail.com>
2024-03-20 14:07:54 +0700

    Integrasi Laravel dengan AdminLte3

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,12 +1,9 @@
 @extends('adminlte::page')
 
-{{-- Extend and customize the browser title --}}
 @section('title')
-    {{ config('adminlte.title') }}
-    @hasSection('subtitle') | @yield('subtitle') @endif
+    {{ config('adminlte.title') }} @hasSection('subtitle') | @yield('subtitle') @endif
 @stop
 
-{{-- Extend and customize the page content header --}}
 @section('content_header')
     @hasSection('content_header_title')
         <h1 class="text-muted">
@@ -21,29 +18,21 @@
     @endif
 @stop
 
-{{-- Rename section content to content_body --}}
 @section('content')
     @yield('content_body')
 @stop
 
-{{-- Create a common footer --}}
 @section('footer')
     <div class="float-right">
         Version: {{ config('app.version', '1.0.0') }}
     </div>
     <strong>
-        KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI
-        POLITEKNIK NEGERI MALANG
-        JURUSAN TEKNOLOGI INFORMASI
-        Jl. Soekarno Hatta No. 9, Jatimulyo, Lowokwaru, Malang 65141
-        Telp. (0341) 404424 - 404425, Fax (0341) 404420
         <a href="{{ config('app.company_url', '#') }}">
             {{ config('app.company_name', 'My company') }}
         </a>
     </strong>
 @stop
 
-{{-- Add common Javascript/Jquery code --}}
 @push('js')
     <script>
         $(document).ready(function() {
@@ -52,7 +41,6 @@
     </script>
 @endpush
 
-{{-- Add common CSS customizations --}}
 @push('css')
     <style type="text/css">
         {{-- You can add AdminLTE customizations here --}}

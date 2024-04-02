61605a929cade6e5653adeb8118f20b33b3fe477
fanesabhirawaning <fanesahbi22@gmail.com>
2024-04-03 00:18:13 +0700

    JOBSHEET 6 : Tugas

--- resources/views/m_user/index.blade.php
+++ resources/views/m_user/index.blade.php
@@ -1,13 +1,11 @@
- @extends('m_user/template')
+@extends('layout.app')
+@section('subtitle', 'User')
+@section('content_header_title', 'CRUD User')
 @section('content')
-    <div class="row mt-5 mb-5">
+    <div class="row mb-3">
         <div class="col-lg-12 margin-tb">
-            <div class="float-left">
-                <h2>CRUD user</h2>
-            </div>
             <div class="float-right">
-                <a class="btn btn-success" href="{{ route('m_user.create') }}"> Input
-                    User</a>
+                <a class="btn btn-success" href="{{ route('m_user.create') }}"><i class="fas fa-plus-circle mr-1"></i> Input User</a>
             </div>
         </div>
     </div>
@@ -16,7 +14,7 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-    <table class="table table-bordered">
+    <table class="table table-bordered table-striped">
         <tr>
             <th width="20px" class="text-center">User id</th>
             <th width="150px" class="text-center">Level id</th>
@@ -25,10 +23,10 @@
             <th width="150px"class="text-center">Password</th>
             <th width="150px" class="text-center">Level Kode</th>
             <th width="150px" class="text-center">Level Nama</th>
+            <th width="150px" class="text-center">Aksi</th>
         </tr>
         @foreach ($useri as $m_user)
             <tr>
-
                 <td>{{ $m_user->user_id }}</td>
                 <td>{{ $m_user->level_id }}</td>
                 <td>{{ $m_user->username }}</td>
@@ -39,12 +37,11 @@
 
                 <td class="text-center" style="white-space: nowrap;">
                     <form action="{{ route('m_user.destroy', $m_user->user_id) }}" method="POST">
-                        <a class="btn btn-info btn-sm" href="{{ route('m_user.show', $m_user->user_id) }}">Show</a>
-                        <a class="btn btn-primary btn-sm" href="{{ route('m_user.edit', $m_user->user_id) }}">Edit</a>
+                        <a class="btn btn-info btn-sm" href="{{ route('m_user.show', $m_user->user_id) }}"><i class="fas fa-eye"></i> </a>
+                        <a class="btn btn-warning btn-sm" href="{{ route('m_user.edit', $m_user->user_id) }}"><i class="fas fa-edit"></i> </a>
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm"
-                            onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
+                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"><i class="fas fa-trash"></i> </button>
                     </form>
                 </td>
             </tr>

6dfb1ceb3f58693f348d20f50a7c05e43770fbae
fanesabhirawaning <fanesahbi22@gmail.com>
2024-05-08 14:31:53 +0700

    JOBSHEET 10 : Membuat restfull API register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,15 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 use App\Models\LevelModel; // Add this import statement
 
-class UserModel extends User
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
-    public $timestamps = false;
+    public $timestamps = true;
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['user_id','level_id','username','nama','password'];

fbf90b247df559205ddaf629186481d28e07e22f
fanesabhirawaning <fanesahbi22@gmail.com>
2024-05-14 15:10:25 +0700

    JOBSHEET 11 : Tugas

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BarangModel;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,30 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
+        //set validator
+        $validator = Validator::make($request->all(), [
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'kategori_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+        //if validations fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
         $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang)
--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,12 +13,14 @@ class BarangModel extends Model
     use HasFactory;
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
+    public $timestamps = true;
     protected $fillable = [
         'barang_kode',
         'barang_nama',
         'harga_beli',
         'harga_jual',
         'kategori_id',
+        'image',
     ];
 
     // Pastikan tipe data stok adalah integer
@@ -33,5 +36,11 @@ class BarangModel extends Model
     public function stok(): HasOne
     {
         return $this->hasOne(StokModel::class, 'barang_id', 'barang_id');
+    }
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
